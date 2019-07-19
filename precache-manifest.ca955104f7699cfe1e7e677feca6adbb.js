537f80dba06ebf7cabf7cb2157ea28331ec8aadc
harpreetkhalsagtbit <harpreetkhalsagtbit@gmail.com>
2019-07-20 00:35:38 +0530

    Updates

---
+++ precache-manifest.ca955104f7699cfe1e7e677feca6adbb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "978cef825f639738d5baf0c0182fe5aa",
+    "url": "/react-swapi/index.html"
+  },
+  {
+    "revision": "0be90f6c938bd203d606",
+    "url": "/react-swapi/static/css/main.202d870d.chunk.css"
+  },
+  {
+    "revision": "0db23fdad86e4a544760",
+    "url": "/react-swapi/static/js/2.70fbd31b.chunk.js"
+  },
+  {
+    "revision": "0be90f6c938bd203d606",
+    "url": "/react-swapi/static/js/main.b8c936f2.chunk.js"
+  },
+  {
+    "revision": "7a1016b3ff399e7649f0",
+    "url": "/react-swapi/static/js/runtime~main.d8b3eecc.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.4765932c1d96b4037018a521ad2b0565.js"
+  "/react-swapi/precache-manifest.ca955104f7699cfe1e7e677feca6adbb.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-swapi/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
